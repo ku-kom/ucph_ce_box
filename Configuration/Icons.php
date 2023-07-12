aef31f135bded8c5209976905c41b94f5c7dc6bc
Nanna Ellegaard <nel@adm.ku.dk>
2023-07-12 15:33:18 +0200

    Setup

---
+++ Configuration/Icons.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * Icon Registry
+ */
+
+defined('TYPO3') || die();
+
+return [
+    // icon identifier
+    'ucph_ce_box_icon' => [
+        'provider' => \TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider::class,
+        'source' => 'EXT:ucph_ce_box/Resources/Public/Icons/box-seam.svg',
+    ],
+];
